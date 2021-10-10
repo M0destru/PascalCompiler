cb44ac47154d58deb49cc04ecec6692b4157693c
M0destru <RustamGhetto@gmail.com>
2021-10-10 20:35:16 +0500

    Input-Output Module

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PascalCompiler
+{
+    class Program
+    {
+        public static void Main()
+        {
+            CIO io = new CIO("./main.pas");
+            CToken cur = null;
+            do
+            {
+                cur = io.GetNextToken();
+                if (cur != null)
+                    Console.WriteLine(cur.ToString());
+            } while (cur != null);
+        }
+    }
+}
+
